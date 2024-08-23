9b07f79a6cb6bf6b104132fba2f37e136f9f365a
Abdi Deresa <Abdishegute@gmail.com>
2024-08-23 11:28:49 -0700

    added a function and a progress bar

--- file.py
+++ file.py
@@ -1,9 +1,17 @@
 from tkinter import*
 from tkinter.ttk import *
 import time
+
+def start():
+    bar['value']+=10
+    
+    
 window= Tk()
 
+bar=Progressbar(window, orient=HORIZONTAL, length=300)
+bar.pack(pady=10)
 
+button = Button(window, text="download", command=start).pack()
 
 
 
