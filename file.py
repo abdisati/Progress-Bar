53db105cedc98babf497940eb300b1360a77d6d5
Abdi Deresa <Abdishegute@gmail.com>
2024-08-23 11:52:48 -0700

    added some more functions

--- file.py
+++ file.py
@@ -9,13 +9,20 @@ def start():
         time.sleep(1)
         bar['value']+=10
         x+=1
+        percent.set(str(int(x/tasks)*100)+"%")
+        text.set(str(x)+"/"+str(tasks)+" tasks completed")
         window.update_idletasks()
     
     
 window= Tk()
+percent = StringVar()
+text=StringVar()
 
 bar=Progressbar(window, orient=HORIZONTAL, length=300)
 bar.pack(pady=10)
+
+percentLabel = Label(window, textvariable=percent).pack()
+taskLabel = Label(window,textvariable=text).pack()
 
 button = Button(window, text="download", command=start).pack()
 
